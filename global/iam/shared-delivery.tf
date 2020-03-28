527c8985240313cf9af80b3eaa5c257e18c66ebc
Stephen Kuenzli <skuenzli@qualimente.com>
2020-03-28 11:57:22 -0700

    Define a `delivery` group for building, testing, and deploying applications.

---
+++ global/iam/shared-delivery.tf
@@ -0,0 +1,32 @@
+# The Delivery group is responsible for building, testing, and deploying infra and applications.
+
+# The model here is that each team will get its own IAM principals for delivery.
+# Teams could be: devops and each project.
+# Separate principals will enable teams to use the most appropriate CI/CD solutions
+# while minimizing risks of lost keys and enabling finer-grained resource policies
+# and permissions boundaries.  Or that's the idea anyway.
+
+resource "aws_iam_user" "delivery-devops" {
+  name = "delivery-devops"
+  path = "/services/"
+}
+
+resource "aws_iam_group" "delivery" {
+  name = "delivery"
+  path = "/services/"
+}
+
+resource "aws_iam_group_policy_attachment" "delivery-Admin" {
+  group      = "${aws_iam_group.delivery.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_group_membership" "delivery" {
+  name = "delivery-membership"
+
+  users = [
+    "${aws_iam_user.delivery-devops.name}",
+  ]
+
+  group = "${aws_iam_group.delivery.name}"
+}
