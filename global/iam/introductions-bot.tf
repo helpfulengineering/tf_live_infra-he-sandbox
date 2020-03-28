0ba2886ed379bb22c263ea2dee739e6f9f5073f9
Stephen Kuenzli <skuenzli@qualimente.com>
2020-03-28 16:12:48 -0700

    Create a delivery-introductions-bot role that is assumable by CloudBuild.

--- global/iam/introductions-bot.tf
+++ global/iam/introductions-bot.tf
@@ -5,3 +5,15 @@ resource "aws_iam_user" "delivery-introductions-bot" {
   path = "/services/"
 }
 
+# introduction bot plans to use CodeBuild; make the delivery role assumable
+
+resource "aws_iam_role" "delivery-introductions-bot" {
+  name = "delivery-introductions-bot"
+  path = "/services/"
+  assume_role_policy = file("${path.module}/templates/assume_role_policy/codebuild.json")
+}
+
+resource "aws_iam_policy_attachment" "delivery-introductions-bot-Admin" {
+  name = "Admin"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
