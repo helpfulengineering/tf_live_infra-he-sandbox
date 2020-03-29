f07bb75e2898bea50111feb067e8f6f818d12ccb
Stephen Kuenzli <skuenzli@qualimente.com>
2020-03-28 17:35:19 -0700

    Define a service role for monitoring-02 project that can be assumed by EC2.

---
+++ global/iam/monitoring-02.tf
@@ -0,0 +1,19 @@
+# Define the O2 monitoring project's principals in sandbox.
+
+# IAM user for CI/CD
+resource "aws_iam_user" "delivery-monitoring-O2" {
+  name = "delivery-monitoring-O2"
+  path = "/services/"
+}
+
+# Service role for the application to run as
+resource "aws_iam_role" "monitoring-O2" {
+  name = "svc-monitoring-O2"
+  path = "/services/"
+  assume_role_policy = file("${path.module}/templates/assume_role_policy/ec2.json")
+}
+
+resource "aws_iam_role_policy_attachment" "monitoring-O2" {
+  role =  aws_iam_role.delivery-introductions-bot.name
+  policy_arn = "arn:aws:iam::aws:policy/PowerUser"
+}
